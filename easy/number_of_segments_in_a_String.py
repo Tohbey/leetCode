90fc80fd16b9727834cf982940ca47052b60479c
tohbey <fafoworatobi25@gmail.com>
2022-04-07 19:53:56 +0100

    number of segments in a string

---
+++ easy/number_of_segments_in_a_String.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        res = s.split()
+        print(len(res))
+
+
+res = Solution()
+s = "Hello, my name is John"
+res.countSegments(s)
