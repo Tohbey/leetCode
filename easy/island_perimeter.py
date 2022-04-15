060f587c3d25d2ab623acc2d5b643c6eddd2a9cf
tohbey <fafoworatobi25@gmail.com>
2022-04-15 23:19:07 +0100

    island perimeter

---
+++ easy/island_perimeter.py
@@ -0,0 +1,11 @@
+from pip import List
+
+
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        print(grid)
+
+
+res = Solution()
+grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+res.islandPerimeter(grid)
