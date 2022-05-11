de95159882ec0a0149454ef165c1b4092eb3d9be
tobey <fafoworatobi25@gmail.com>
2022-05-11 11:27:28 +0100

    spiral matrix II solution

--- medium/spiral_matrix_II.py
+++ medium/spiral_matrix_II.py
@@ -3,8 +3,39 @@ from pip import List
 
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        rows, cols = (n,n)
-        matrix = [[0]* cols]*rows
+        matrix = [['*' for _ in range(n)] for _ in range(n)]
+        row, col = 0, 0
+        num = 1
+        while num <= n*n:
+            while col < n and matrix[row][col] == '*':
+                matrix[row][col] = num
+                num += 1
+                col += 1
+            col -= 1
+            row += 1
+            
+            while row < n and matrix[row][col] == '*':
+                matrix[row][col] = num
+                num += 1
+                row += 1
+            row -= 1
+            col -= 1
+            
+            while col >= 0 and matrix[row][col] == '*':
+                print("Left : ",num)
+                matrix[row][col] = num
+                num += 1
+                col -= 1
+            col += 1
+            row -= 1
+            
+            while row >= 0 and matrix[row][col] == '*':
+                matrix[row][col] = num
+                num += 1
+                row -= 1
+            row += 1
+            col += 1
+        return matrix
        
 
 n = 3
