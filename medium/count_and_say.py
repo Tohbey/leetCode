22e09780a956d902867a655f6a83ccc4d22ba24c
tobey <fafoworatobi25@gmail.com>
2022-05-20 11:33:49 +0100

    count and say

--- medium/count_and_say.py
+++ medium/count_and_say.py
@@ -1,3 +1,19 @@
+from itertools import groupby
+
+
 class Solution:
     def countAndSay(self, n: int) -> str:
+        memo = ["1"]
+        for i in range(1, n):
+            num = self._calc(memo[i-1])
+            memo.append(num)
         
+        return memo[n-1]
+    
+    def _calc(self, seq: str) -> str:
+        result = ""
+        for num, group in groupby(seq):
+            count = len(list(group))
+            result += f"{count}{num}"
+        
+        return result
