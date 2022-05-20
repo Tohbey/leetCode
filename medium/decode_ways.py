611d0d3a87bcbb569347724abea9b0ccdb8ee9fc
tobey <fafoworatobi25@gmail.com>
2022-05-20 10:29:32 +0100

    decode ways

--- medium/decode_ways.py
+++ medium/decode_ways.py
@@ -10,8 +10,7 @@ class Solution:
                 return 0
 
             res = dfs(i+1)
-            if (i + 1 < len(s) and (s[i] == "1"
-                                    or s[i] == "2" and s[i+1] in "0123456")):
+            if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
                 res += dfs(i+2)
 
             dp[i] = res
@@ -28,8 +27,7 @@ class Solution:
             else:
                 dp[i] = dp[i+1]
             
-            if (i + 1 < len(s) and (s[i] == "1"
-                                    or s[i] == "2" and s[i+1] in "0123456")):
+            if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
                 dp[i] += dp[i+2]
                 
 
