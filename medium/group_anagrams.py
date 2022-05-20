e541409f5e065b7407e550ba6bd424a2244ad8db
tobey <fafoworatobi25@gmail.com>
2022-05-20 11:34:01 +0100

    group anagrams

--- medium/group_anagrams.py
+++ medium/group_anagrams.py
@@ -1,13 +1,20 @@
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = []
-        if len(strs) == 1:
-            return result.append(strs)    
+        result = defaultdict(list) #mapping the charcount to the list of anagrams
+        
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+                
+            result[tuple(count)].append(s)
 
-
+        return result.values()
+    
 res = Solution()
 strs = ["eat","tea","tan","ate","nat","bat"]
 res.groupAnagrams(strs)
