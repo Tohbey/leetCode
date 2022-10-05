d3bc463700a5f262e7ce200b93831fd3837112d7
tobey <fafoworatobi25@gmail.com>
2022-10-05 18:52:41 +0100

    surrounded regions

---
+++ medium/surrounded_regions.py
@@ -0,0 +1,39 @@
+from pip import List
+
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+        
+        for i in [0, len(board)-1]:
+            for j in range(len(board[0])):
+                self.dfs(board,i,j)
+                                
+        for i in range(len(board)):
+            for j in [0, len(board[0])-1]:
+                self.dfs(board, i, j)
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == '.':
+                    board[i][j] = 'O'
+                            
+        
+    def dfs(self, board, i, j):
+        if 0<=i<len(board) and 0<=j<len(board[0]) and board[i][j] == 'O':
+            board[i][j] = '.'
+            self.dfs(board, i+1, j)
+            self.dfs(board, i-1, j)
+            self.dfs(board, i, j+1)
+            self.dfs(board, i, j-1)
+            
+
+                
+    
+                
+boards = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+res = Solution()
+res.solve(boards)
